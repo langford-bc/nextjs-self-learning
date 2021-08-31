0bd8bc5f672ec96d63ca9a3283f8b1a5c71d485a
langford-bc <sk283@caa.columbia.edu>
2021-08-31 23:14:03 +0200

    basic page with data from API

---
+++ components/Header.js
@@ -0,0 +1,10 @@
+
+
+export default Header = () => {
+  <div>
+
+      <title>My page title</title>
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+    </Head>
+  </div>
+}
---
+++ pages/index.js
@@ -0,0 +1,28 @@
+import Head from 'next/head'
+import styles from '../styles/home.module.css'
+
+export async function getStaticProps() {
+  const response = await fetch('https://jsonplaceholder.typicode.com/posts/?_limit=10')
+  const posts = await response.json()
+  return {
+    props: {
+      posts
+    }, // will be passed to the page component as props
+  }
+}
+
+// posts will be populated at build time by getStaticProps()
+function Blog({ posts }) {
+  return (
+    <>
+    <h1>This is a blog with content from an API</h1>
+      <ul>
+        {posts.map((post) => (
+          <li>{post.title}</li>
+        ))}
+      </ul>
+    </>
+  )
+}
+
+export default Blog
